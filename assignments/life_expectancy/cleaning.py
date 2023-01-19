45483bcfd4314154ae913770a4b9d1d9a7988b23
susana almeida <susana.almeida@nos.pt>
2023-01-19 10:05:13 +0000

    Review

--- assignments/life_expectancy/cleaning.py
+++ assignments/life_expectancy/cleaning.py
@@ -3,19 +3,21 @@
 
 import pandas as pd
 
-def clean_data(input_file: str, output_file: str, region: str='PT') -> None:
 
-    # read table
-    data = pd.read_csv('/nfs/workstation/wb_mkte/scalmeida/FAAST_SCA/assignments/life_expectancy/data/eu_life_expectancy_raw.tsv',sep='\t',engine ='python')
+def clean_data() -> None:
 
-    # extract only the values that match the pattern of digits and decimal points
-    data.value=data.value.str.extract(r"(\d+\.\d+)")
+# read table
+
+    file_path = '/nfs/workstation/wb_mkte/scalmeida/FAAST_SCA/assignments/life_expectancy/data/eu_life_expectancy_raw.tsv'
+    with open(file_path, 'r') as file:
+        data = pd.read_csv(file, sep='\t', engine='python') 
     
+
     # create new columns
     data = data.assign(unit=data["unit,sex,age,geo\\time"].str.split(',').str[0],
-                sex=data["unit,sex,age,geo\\time"].str.split(',').str[1],
-                age=data["unit,sex,age,geo\\time"].str.split(',').str[2],
-                geo=data["unit,sex,age,geo\\time"].str.split(',').str[3])
+                    sex=data["unit,sex,age,geo\\time"].str.split(',').str[1],
+                    age=data["unit,sex,age,geo\\time"].str.split(',').str[2],
+                    geo=data["unit,sex,age,geo\\time"].str.split(',').str[3])
 
     # rename column geo to region
     data = data.rename(columns={"geo":"region"})
@@ -29,7 +31,8 @@ def clean_data(input_file: str, output_file: str, region: str='PT') -> None:
     var_name = 'year',
     value_name = 'value')
 
-    # year is an int 
+    # extract only the values that match the pattern of digits and decimal points, year is an int
+    data.value=data.value.str.extract(r"(\d+\.\d+)")
     data['year'] = data['year'].astype(int)
 
     # remove the NaN and trasform 
@@ -44,5 +47,6 @@ def clean_data(input_file: str, output_file: str, region: str='PT') -> None:
     # select region
     data = data.query('region == "PT"')
 
-    print (data)
+   
     data.to_csv('/nfs/workstation/wb_mkte/scalmeida/FAAST_SCA/assignments/life_expectancy/data/pt_life_expectancy.csv', index=False)
+   
