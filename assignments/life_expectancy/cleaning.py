17d54c624044c7551a4561dc86d2df26db75a4bc
susana almeida <susana.almeida@nos.pt>
2023-01-19 11:22:19 +0000

    Update

--- assignments/life_expectancy/cleaning.py
+++ assignments/life_expectancy/cleaning.py
@@ -1,17 +1,16 @@
-
+"""Training Clean Data"""
 ## Assignmnents 1 - cleaning.py
 
+import argparse
 import pandas as pd
 
-
-def clean_data() -> None:
-
+def clean_data(region:str) -> None:
+    """ Clean data  """
 # read table
 
-    file_path = '/nfs/workstation/wb_mkte/scalmeida/FAAST_SCA/assignments/life_expectancy/data/eu_life_expectancy_raw.tsv'
-    with open(file_path, 'r') as file:
-        data = pd.read_csv(file, sep='\t', engine='python') 
-    
+    file_path = './life_expectancy/data/eu_life_expectancy_raw.tsv'
+    with open(file_path, 'r', encoding="utf-8") as file:
+        data = pd.read_csv(file, sep='\t', engine='python')
 
     # create new columns
     data = data.assign(unit=data["unit,sex,age,geo\\time"].str.split(',').str[0],
@@ -23,10 +22,12 @@ def clean_data() -> None:
     data = data.rename(columns={"geo":"region"})
 
     # create array year
-    column_year = data.columns[~data.columns.isin(['unit,sex,age,geo\\time','unit','sex','age','region'])]
+    column_year = data.columns[
+        ~data.columns.isin(['unit,sex,age,geo\\time','unit','sex','age','region'])]
 
-    #Unpivots the date to long format, so that we have the following columns: unit, sex, age, region, year, value
-    unpivoted_df = data = data.melt(id_vars=['unit','sex','age','region'],
+    # unpivots the date to long format,
+    # following columns: unit, sex, age, region, year, value
+    data = data.melt(id_vars=['unit','sex','age','region'],
     value_vars= column_year,
     var_name = 'year',
     value_name = 'value')
@@ -35,7 +36,7 @@ def clean_data() -> None:
     data.value=data.value.str.extract(r"(\d+\.\d+)")
     data['year'] = data['year'].astype(int)
 
-    # remove the NaN and trasform 
+    # remove the NaN and trasform
     data = data.dropna()
     data=data[(data.value.str.strip())!="NaN"]
     data=data[(data.value.notnull())]
@@ -45,8 +46,15 @@ def clean_data() -> None:
     data['value'] = data['value'].astype(float)
 
     # select region
-    data = data.query('region == "PT"')
+    data = data.query(f'region == "{region}"')
 
-   
-    data.to_csv('/nfs/workstation/wb_mkte/scalmeida/FAAST_SCA/assignments/life_expectancy/data/pt_life_expectancy.csv', index=False)
-   
+    data.to_csv('./life_expectancy/data/pt_life_expectancy.csv', index=False)
+
+if __name__ == "__main__":  # pragma: no cover
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--region",
+    help="region is a code to use on clean data",
+    default="PT",required=False)
+    args = parser.parse_args()
+
+    clean_data(args.region)
