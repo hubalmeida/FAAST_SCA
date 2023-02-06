d66c186ab5d8973d7cff96fb854dd0db91a19ac6
susana almeida <susana.almeida@nos.pt>
2023-02-06 11:44:47 +0000

    refactoring_update_rev

--- assignments/life_expectancy/cleaning.py
+++ assignments/life_expectancy/cleaning.py
@@ -3,76 +3,46 @@
 ## Assignmnents 2
 
 import argparse
-import pathlib
-from pathlib import Path
 import pandas as pd
-from pandas import DataFrame
+from pathlib import Path
 
-def load_data ():
+def load_data():
     """ function to load data and return the data to clean, dataclean"""
-    file_path = Path(__file__).parent / "data"/"eu_life_expectancy_raw.tsv"
-    with open(file_path, 'r', encoding="utf-8") as file:
-        dataclean= pd.read_csv(file, sep='\t', engine='python')
-        return dataclean
+    file_path = Path(__file__).parent / "data/eu_life_expectancy_raw.tsv"
+    return pd.read_csv(file_path, sep='\t', encoding="utf-8")      
 
-def clean_data(region:str, dataclean:DataFrame):
+def clean_data(dataclean, region):
     """ function clean data, receives dataclean"""
     # create new columns
     dataclean = dataclean.assign(unit=dataclean["unit,sex,age,geo\\time"].str.split(',').str[0],
-                    sex=dataclean["unit,sex,age,geo\\time"].str.split(',').str[1],
-                    age=dataclean["unit,sex,age,geo\\time"].str.split(',').str[2],
-                    geo=dataclean["unit,sex,age,geo\\time"].str.split(',').str[3])
-
-    # rename column geo to region
-    dataclean = dataclean.rename(columns={"geo":"region"})
-
-    # create array year
-    column_year = dataclean.columns[
-        ~dataclean.columns.isin(['unit,sex,age,geo\\time','unit','sex','age','region'])]
-
-    # unpivots the date to long format,
-    # following columns: unit, sex, age, region, year, value
+                                 sex=dataclean["unit,sex,age,geo\\time"].str.split(',').str[1],
+                                 age=dataclean["unit,sex,age,geo\\time"].str.split(',').str[2],
+                                 region=dataclean["unit,sex,age,geo\\time"].str.split(',').str[3])
+    dataclean = dataclean.rename(columns={"region": "region"})
     dataclean = dataclean.melt(id_vars=['unit','sex','age','region'],
-    value_vars= column_year,
-    var_name = 'year',
-    value_name = 'value')
-
-    # extract only the values that match the pattern of digits and decimal points, year is an int
-    dataclean.value=dataclean.value.str.extract(r"(\d+\.\d+)")
+                                value_vars=dataclean.columns[~dataclean.columns.isin(
+                                    ['unit,sex,age,geo\\time','unit','sex','age','region'])],
+                                var_name='year', value_name='value')
+    dataclean.value = dataclean.value.str.extract(r"(\d+\.\d+)")
     dataclean['year'] = dataclean['year'].astype(int)
-
-    # remove the NaN and trasform
-    #dataclean = dataclean.dropna()
-    dataclean=dataclean[(dataclean.value.str.strip())!="NaN"]
-    dataclean=dataclean[(dataclean.value.notnull())]
+    dataclean = dataclean[(dataclean.value.str.strip() != "NaN") & (dataclean.value.notnull())]
     dataclean['value'] = pd.to_numeric(dataclean['value'], errors='coerce')
-
-    # value float
     dataclean['value'] = dataclean['value'].astype(float)
+    return dataclean[dataclean['region'] == region].dropna()
 
-    # select region
-    #dataclean = dataclean.query(f'region == "{region}"')
-    cleaned_data = dataclean.dropna()
-    #data = dataclean.query(f'region == "{region}"')
-    data = cleaned_data[cleaned_data['region'] == region]
-    return data
 
-def save_data(datasave: DataFrame):
-    """ function save data"""
-# define a path and write to csv
-    out_path = pathlib.Path(__file__).parent / 'data/pt_life_expectancy.csv'
- 
-#write to csv
+def save_data(datasave):
+    out_path = Path(__file__).parent / 'data/pt_life_expectancy.csv'
     datasave.to_csv(out_path, index=False)
-    data = load_data()
-    cleaned_data = clean_data(data)
+
+def main(region='PT'):
+    dataclean = load_data()
+    cleaned_data = clean_data(dataclean, region)
     save_data(cleaned_data)
 
-def main(region: str):
-
-
-    if __name__ == "__main__": # pragma: no cover
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-region',"--region",type=str,
-        help="region is a code to use on clean data",default='PT',required=False)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-region', "--region", type=str,
+                        help="Region is a code to use on clean data", default='PT', required=False)
     args = parser.parse_args()
+    main(args.region)
